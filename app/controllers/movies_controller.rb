5fd4e8d8da4b5c47f90a684864695bcb14b1b2bb
tojawawel <bgawel93@gmail.com>
2018-12-03 15:39:42 +0100

    add index layout and movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,48 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only:[:show,:edit,:update,:destroy]
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  private
+  def movie_params
+    movie_params = params.require(:movie.permit(:name, :premiere_date,:length, :description, :genre, :director, :scenarist, :country))
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
 end
