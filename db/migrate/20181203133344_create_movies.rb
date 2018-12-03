2c9f2e0fd5f5a45292e6861699af353dbbf0e652
tojawawel <bgawel93@gmail.com>
2018-12-03 14:35:12 +0100

    create movie model

---
+++ db/migrate/20181203133344_create_movies.rb
@@ -0,0 +1,16 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.datetime :premiere_date
+      t.string :length
+      t.body :description
+      t.string :genre
+      t.string :director
+      t.string :scenarist
+      t.string :country
+
+      t.timestamps
+    end
+  end
+end
