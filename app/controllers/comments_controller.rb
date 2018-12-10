132f9b93400e35a8ba4d7ca81319ed10bf277306
tojawawel <bgawel93@gmail.com>
2018-12-10 10:38:16 +0100

    add comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,44 @@
+class CommentsController < ApplicationController
+  before_action :find_movie
+  before_action :find_comment, only: [:destroy, :edit, :update]
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.movie = @movie
+    if @comment.save
+      redirect_to movie_path(@comment.movie)
+    else
+      render 'movies/show'
+    end
+
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to movie_path(@movie)
+  end
+
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to movie_path(@movie)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:author,:rate, :body, :movie_id)
+  end
+
+  def find_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+
+  def find_comment
+    @comment = Comment.find(params[:id])
+  end
+end
---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+  validates :author ,presence: true
+  validates :rate ,presence: true, numericality: { greater_than:0,less_or_equal_than:10,  only_integer: true }
+  validates :body ,presence: true, length: { minimum:5}
+  belongs_to :movie
+
+end
